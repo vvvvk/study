2aaaa9d636210b757343387eec16a7bdcecdde1d
Vasiliy Kryuchkov <vvvvk@yandex.ru>
2020-05-11 18:38:08 +0400

    fp2 solved

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,27 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        length.times { |i| yield(self[i]) }
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map
+        my_reduce(MyArray.new) { |acc, el| acc << yield(el) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(MyArray.new) { |acc, el| el.nil? ? acc : acc << el }
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(acc = nil)
+        acc, *rest = acc.nil? ? self : [acc, *self]
+        rest.length.times { |i| acc = yield(acc, rest[i]) }
+        acc
+      end
     end
   end
 end
